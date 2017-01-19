8df7c3d1c305473f90b5b0bad81d1834618fe36d
Manju <manjunath.064@gmail.com>
2017-01-19 10:15:44 +0100

    REST Api initial setup

---
+++ RESTApi/Models/candidateModel.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;;
+
+var candidateModel = new Schema(
+    {
+        firstName:String,
+        lastName:String,
+        gender:String,
+        phone:Number,
+        email:String,
+        highestQualification:String,
+        linkedInUrl:String,
+        comment:String,
+        PreferredLocations:{ type: Array, 'default': [] },
+        RoleInterestedId:Number,
+        SectorId:Number,
+        SubscribeToNewsLetter:{ type: Boolean, default:false },
+        PrivacyDisclaimer:{ type: Boolean, default:false },
+        EventId:Number
+    }
+);
+/*
+candidateModel.virtual('candidateId').get(function() {
+    return this._id;
+});
+*/
+var collectionName = 'candidates'
+module.exports = mongoose.model('Candidates', candidateModel,collectionName);
---
+++ RESTApi/app.js
@@ -0,0 +1,45 @@
+var express = require('express'),
+    mongoose = require('mongoose');
+
+var dbUrl = 'mongodb://localhost/recruitment';
+
+var conn = mongoose.connect(dbUrl, {user: 'CGI', pwd: 'CGIEvents2017'},
+                            function(err, db) {
+                                if (err) {
+                                    console.log('Unable to connect to the mongoDB server Err', err);
+                                } else {
+                                    //We are connected. :)
+                                    console.log('Connection established to', dbUrl); 
+                            }
+                    });
+
+var Candidate = require('./models/candidateModel');
+
+//var Candidate = conn.model('Candidate', candidateSchema);
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+var candidateRouter = express.Router();
+
+candidateRouter.route('/Candidates')
+        .get(function(req,res){
+            Candidate.find(function(err, results){
+                if(err){
+                    console.log("Error in getting Candidates err " + JSON.stringify(err));
+                }else{
+                    //console.log("Response Candidates " + JSON.stringify(results ));
+                    res.json(results);
+                }
+            });
+        });
+
+app.use('/api', candidateRouter);
+
+app.get('/', function(req, res){
+    res.send('Candidae API Started');
+});
+
+app.listen(port, function(){
+    console.log("Gulp is Running on PORT " + port);
+});
