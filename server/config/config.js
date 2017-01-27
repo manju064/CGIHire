8409983b5fbcdd02d8a91b7bda76be05b2d909dd
manju <manjunath.keshava@cgi.com>
2017-01-27 12:00:46 +0100

    Updated config for external config

--- server/config/config.js
+++ server/config/config.js
@@ -5,19 +5,19 @@ module.exports = {
   development: {
     rootPath: rootPath,
     db:{
-        user:"CGI",
-        pwd: "CGIEvents2017",
-        url:'mongodb://ds117109.mlab.com:17109/recruitment',
+        user: process.env.DB_USER || "CGI",
+        pwd: process.env.DB_PWD || "CGIEvents2017",
+        url: process.env.DB_URL || 'mongodb://ds117109.mlab.com:17109/recruitment',
     },
-    port: process.env.PORT || 80
+    port: process.env.PORT || 8081
   },
   production: {
     rootPath: rootPath,
     //db: 'mongodb://CGI:CGIEvents2017@ds117109.mlab.com:17109/recruitment',
     db: {
-        user:"CGI",
-        pwd: "CGIEvents2017",
-        url:"mongodb://104.198.145.48:27017/recruitment",
+        user: process.env.DB_USER || "CGI",
+        pwd: process.env.DB_PWD || "CGIEvents2017",
+        url: process.env.DB_URL || "mongodb://ds117109.mlab.com:17109/recruitment",
     },
     port: process.env.PORT || 80
   }
