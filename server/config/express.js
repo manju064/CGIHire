678811320d14dbefdca8c390ccca9cc1348ee813
manju <manjunath.keshava@cgi.com>
2017-01-30 14:42:48 +0100

    More logs added

--- server/config/express.js
+++ server/config/express.js
@@ -13,14 +13,20 @@ module.exports = function(app, config) {
     return stylus(str).set('filename', path);
   }
 
- var clientAppPath = path.normalize(__dirname + '/../../public/client/');
+  var clientAppPath = path.normalize(__dirname + '/../../public/client/');
+
+  console.log('express clientAppPath ' + clientAppPath);
 
   app.set('views', config.rootPath + '/client/app/assets/views');
   app.engine('html', cons.swig)
   app.set('view engine', 'html');
 
+  console.log('express view path config complete');
+
   app.use('port', process.env.PORT || config.port);
   app.use('base url', process.env.URL || 'http://localhost');
+
+  console.log('express port and url config complete');
 
   app.use(express.static(path.join(__dirname, '/../../public/client')));
   //app.use(express.static(__dirname + '/../../public'));
@@ -29,15 +35,19 @@ module.exports = function(app, config) {
   app.use('/assets', express.static(clientAppPath + '/app/assets'));
   app.use('/bin', express.static(__dirname + '/../../public/bin'));
   //app.use(favicon(config.clientAppPath  + '/assets/images/favicon.ico'));
-
+  
+  console.log('express static config complete');
+  
   app.use(logger('dev'));
   app.use(cookieParser());
 
+  console.log('express static config complete');
+  
   app.use(bodyParser.urlencoded({extended:true}));
   app.use(bodyParser.json());
 
   //TODO, required?
-  var allowCrossDomain = function(req, res, next) {
+  /*var allowCrossDomain = function(req, res, next) {
                         res.header('Access-Control-Allow-Origin', '*');
                         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
                         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -45,4 +55,5 @@ module.exports = function(app, config) {
                       };
 
   app.use(allowCrossDomain);
+  */
 }
