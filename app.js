3dab9aa2fa1713679e78a2c4fe6f77f09f8279c6
manju <manjunath.keshava@cgi.com>
2017-01-30 14:04:00 +0100

    Added logs, removed gulpFile(temp fix)

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 var express = require('express');
 
-var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var app = express();
+console.log('express library loaded ');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 console.log('env ' + env);
 
+var app = express();
+
+console.log('express instances created ');
+
 var config = require('./server/config/config')[env];
+
+console.log('config loaded ');
 
 require('./server/config/express')(app, config);
 
+console.log('express app loaded ');
+
 require('./server/config/mongoose')(config);
 
+console.log('mongoose loaded ');
+
 require('./server/config/routes')(app, config);
+
+console.log('routes loaded ');
 
 app.listen(config.port);
 console.log('Listening on port ' + config.port + '...');
