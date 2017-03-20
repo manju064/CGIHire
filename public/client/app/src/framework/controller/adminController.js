64edd60078afcfd08032f841fc7f45d4a9cb6b53
Manju <manjunath.064@gmail.com>
2017-03-20 04:54:07 +0100

    Report skills issue fixed.

--- public/client/app/src/framework/controller/adminController.js
+++ public/client/app/src/framework/controller/adminController.js
@@ -12,7 +12,7 @@
              * @constructor
              */
             var adminController = function($scope, $filter, $uibModal, uiGridConstants, 
-                                    candidateService, dataService, $translate)
+                                    candidateService, dataService, $translate,lodash)
             {
                 // #region init, 
                 console.log("admin Controller Initialized");
@@ -33,7 +33,6 @@
 
                 //TODO, problem with sync
                 $scope.GetAllCandidates();
-                
                 // #endregion    
 
                 //csv file needs data in format
@@ -41,21 +40,38 @@
                 $scope.fileName = $translate.instant('RegisteredCandidatesFileName') 
                                     + $filter('date')(new Date(), 'yyyyMMddHHmm') + '.csv';
 
+                $scope.skills =[];
+                dataService.get('skills').then( (data) => {
+                    angular.extend($scope.skills, data);
+                }, (err) => {
+                        console.log('skills err ' + JSON.stringify(err));
+                });            
+
                 $scope.getExportData = function(){
                     candidateService.getFormatedData().then( (data) => {
                         console.log("getFormatedData data = " + JSON.stringify(data));
                         angular.extend($scope.exportData, data);
 
+                        lodash.forEach($scope.exportData, function(row) {
+                            if(row.skills.length >0){
+                                var skillsText = []
+                                 lodash.forEach(row.skills, function(item) {
+                                    var skill = lodash.find($scope.skills, ['id', item.id]);
+                                    //console.log('id ' + item.id + 'skill ' + skill.label);
+                                    skillsText.push(skill.label);
+                                });
+                                console.log('skillsText ' + skillsText.join(","));
+                                row.skillsText = skillsText.join(",");
+                             }
+                        });
                     }, (err) => {
                             console.log('getFormatedData err ' + JSON.stringify(err));
                     });
                 }
-               
-
 
                 $scope.getExportData();
                 $scope.reportHeader = [ "firstName", "lastName", "languages","emailId", "phoneNumber", "highestQualification", 
-                                        "qualificationDate",  'currentRole',"role", "certification","certificationName"
+                                        "qualificationDate",  'currentRole',"role","skillsText","certification","certificationName"
                                         ,"potential", "linkedInUrl", "preferredLocation",
                                          "subscribeToNewsLetter", "privacyDisclaimer", "sector", "cgiContact", "comment"];
 
@@ -68,6 +84,7 @@
                                         ,$translate.instant('Graduation_Date')
                                         ,$translate.instant('CurrentRole')
                                         ,$translate.instant('role')
+                                        ,$translate.instant('skills')
                                         ,$translate.instant('certification')
                                         ,$translate.instant('Certification_Name_Hint')
                                         ,$translate.instant('potential')
@@ -140,7 +157,7 @@
 
             };
 
-            return ["$scope", '$filter','$uibModal', 'uiGridConstants','candidateService', 'dataService', '$translate', adminController];
+            return ["$scope", '$filter','$uibModal', 'uiGridConstants','candidateService', 'dataService', '$translate', 'lodash', adminController];
         });
 
 }( define ));
